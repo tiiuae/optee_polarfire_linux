7fc7dfd84845f3b3f1d960140844e73fe10c96d9
Conor Dooley <conor.dooley@microchip.com>
2021-11-18 11:56:38 +0000

    i2c: rename driver/kconfig to microchip-core
    Deprecate the use of Microsemi in our codebase and standardise
    defconfigs/driver naming to either Microchip or MPFS related strings.
    In this case, use "microchip-core" to identify that it is the driver
    for corei2c and derived peripherals

--- drivers/i2c/busses/i2c-microchip-core.c
+++ drivers/i2c/busses/i2c-microchip-core.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Microchip I2C controller driver
+ * Microchip corei2c I2C controller driver
  *
  * Copyright (c) 2018 - 2021 Microchip Corporation. All rights reserved.
  *
