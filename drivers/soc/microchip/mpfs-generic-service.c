38627e0d6caf47f92b483f6be9ec32f49f8d172d
Conor Dooley <conor.dooley@microchip.com>
2022-02-11 08:42:40 +0000

    soc/microchip: reorder operations in generic service
    Previously the driver read in a bunch of data and executed a transaction
    each and every time a read was done. instead, move the transaction to
    the write function and allow only a single read per write. Also use
    devm_ version allocations/frees.
    
    This has the side effect of also clearing an kernel oops, probably
    caused by the a lack of mallocs.

--- drivers/soc/microchip/mpfs-generic-service.c
+++ drivers/soc/microchip/mpfs-generic-service.c
@@ -2,7 +2,7 @@
 /*
  * Microchip MPFS generic_service driver
  *
- * Copyright (c) 2020 Microchip Corporation. All rights reserved.
+ * Copyright (c) 2020-2022 Microchip Corporation. All rights reserved.
  *
  * Author: Conor Dooley
  *
@@ -21,91 +21,114 @@ static DEFINE_MUTEX(mpfs_generic_service_mutex);
 
 struct mpfs_generic_service_priv {
 	struct mpfs_sys_controller *sys_controller;
-	u8 *buffer;
-	size_t len;
-	u8 cmd_opcode;
-	u16 cmd_size_bytes;
-	u16 resp_size_bytes;
-	u16 mbox_offset;
-	u16 resp_offset;
+	struct mpfs_mss_response *response;
+	struct device *dev;
+	u8 *response_msg;
 };
 
 struct mpfs_generic_service_priv *generic_service_priv;
 
-static ssize_t mpfs_generic_service_write(struct file *filep, const char __user *userbuf,
-				    size_t len, loff_t *offset)
+static inline void mpfs_generic_service_free_message_structs(void)
 {
+	devm_kfree(generic_service_priv->dev, generic_service_priv->response->resp_msg);
+	devm_kfree(generic_service_priv->dev, generic_service_priv->response);
+	generic_service_priv->response = NULL;
+}
+
+static ssize_t mpfs_generic_service_write(struct file *filep, const char __user *userbuf,
+					  size_t len, loff_t *offset)
+{
+	struct mpfs_mss_response *response;
 	u8 *cmd_data;
+	int ret;
 
 	if (len < 9U || len > 4096U)
 		return -1;
 
-	cmd_data = kmalloc(len, GFP_KERNEL);
+	cmd_data = devm_kmalloc(generic_service_priv->dev, len, GFP_KERNEL);
 	if (!cmd_data)
 		return -ENOMEM;
 
-	int ret = copy_from_user(cmd_data, userbuf, len);
-	if (ret)
+	ret = copy_from_user(cmd_data, userbuf, len);
+	if (ret) {
+		devm_kfree(generic_service_priv->dev, cmd_data);
 		return -ret;
+	}
 
-	generic_service_priv->buffer = cmd_data+9;
-	generic_service_priv->len = len - 9;
+	if(generic_service_priv->response != NULL)
+		mpfs_generic_service_free_message_structs();
 
-	generic_service_priv->cmd_opcode = *(cmd_data);
-	generic_service_priv->cmd_size_bytes = (u16)(*(cmd_data + 1)) + (u16)(*(cmd_data + 2) << 8);
-	generic_service_priv->resp_size_bytes =
-		ALIGN((u16)(*(cmd_data + 3)) + ((u16)(*(cmd_data + 4) << 8)), (4U));
-	generic_service_priv->mbox_offset = (u16)(*(cmd_data + 5)) + ((u16)(*(cmd_data + 6) << 8));
-	generic_service_priv->resp_offset = (u16)(*(cmd_data + 7)) + ((u16)(*(cmd_data + 8) << 8));
+	response = devm_kmalloc(generic_service_priv->dev, sizeof(struct mpfs_mss_response), GFP_KERNEL);
+	if (!response) {
+		devm_kfree(generic_service_priv->dev, cmd_data);
+		return -ENOMEM;
+	}
+
+	response->resp_status = 0U;
+	response->resp_size = ALIGN((u16)(*(cmd_data + 3)) + ((u16)(*(cmd_data + 4) << 8)), (4U));
+	response->resp_msg  = (u32 *)devm_kzalloc(generic_service_priv->dev, response->resp_size*sizeof(u8), GFP_KERNEL);
+	if (!response->resp_msg ) {
+		devm_kfree(generic_service_priv->dev, cmd_data);
+		devm_kfree(generic_service_priv->dev, response);
+		return -ENOMEM;
+	}
+
+	generic_service_priv->response = response;
+
+	struct mpfs_mss_msg msg = {
+		.cmd_opcode = *(cmd_data), // do i need to check what the opcode is?
+		.cmd_data_size = (u16)(*(cmd_data + 1)) + (u16)(*(cmd_data + 2) << 8),
+		.response = generic_service_priv->response,
+		.cmd_data = cmd_data+9,
+		.mbox_offset = (u16)(*(cmd_data + 5)) + ((u16)(*(cmd_data + 6) << 8)),
+		.resp_offset = (u16)(*(cmd_data + 7)) + ((u16)(*(cmd_data + 8) << 8))
+	};
+
+	if (msg.cmd_data_size != len-9)
+		return -EINVAL;
+
+	ret = mpfs_blocking_transaction(generic_service_priv->sys_controller, &msg);
+	devm_kfree(generic_service_priv->dev, cmd_data);
+	if (ret) {
+		mpfs_generic_service_free_message_structs();
+		return -EIO;
+	}
 
 	return len;
 }
 
 static ssize_t mpfs_generic_service_read(struct file *filp, char __user *userbuf,
-				     size_t len, loff_t *f_pos)
+					 size_t len, loff_t *f_pos)
 {
-	u8 *response_msg;
-	u8 buffer[2 * generic_service_priv->resp_size_bytes + 3];
-	u8 *cmd_data = generic_service_priv->buffer;
-	u8 *bufferp = buffer;
-	u32 i;
+	u8 *buffer, *bufferp, *response_msg;
+	int i, ret;
 
-	response_msg = kzalloc(sizeof(*response_msg) * generic_service_priv->resp_size_bytes, GFP_KERNEL);
-	if (!response_msg)
+	if(!generic_service_priv->response)
+		return -ENOMSG;
+
+	response_msg = (u8 *)generic_service_priv->response->resp_msg;
+	buffer = devm_kmalloc(generic_service_priv->dev, 2 * generic_service_priv->response->resp_size + 3, GFP_KERNEL);
+
+	if(!buffer) {
+		mpfs_generic_service_free_message_structs();
 		return -ENOMEM;
+	}
 
-	struct mpfs_mss_response response = {
-		.resp_status = 0U,
-		.resp_msg = (u32 *)response_msg,
-		.resp_size = generic_service_priv->resp_size_bytes
-	};
+	bufferp = buffer;
 
-	struct mpfs_mss_msg msg = {
-		.cmd_opcode = generic_service_priv->cmd_opcode,
-		.cmd_data_size = generic_service_priv->cmd_size_bytes,
-		.response = &response,
-		.cmd_data = cmd_data,
-		.mbox_offset = generic_service_priv->mbox_offset,
-		.resp_offset = generic_service_priv->resp_offset
-	};
-
-	if (!generic_service_priv->buffer || generic_service_priv->len != generic_service_priv->cmd_size_bytes)
-		return -EFAULT;
-
-	int ret = mpfs_blocking_transaction(generic_service_priv->sys_controller, &msg);
-
-	if (ret)
-		return -EIO;
-	bufferp += sprintf(bufferp, "%02x ", response.resp_status);
-	for (i = 0; i < generic_service_priv->resp_size_bytes; i++)
+	bufferp += sprintf(bufferp, "%02x ", generic_service_priv->response->resp_status);
+	for (i = 0; i < generic_service_priv->response->resp_size; i++){
 		bufferp += sprintf(bufferp, "%02x", response_msg[i]);
+	}
+	bufferp += sprintf(bufferp, "\0");
 
-	kfree(generic_service_priv->buffer);
-	kfree(response_msg);
+	ret = simple_read_from_buffer(userbuf, len, f_pos, buffer,
+				      2 * generic_service_priv->response->resp_size + 3);
 
-	return simple_read_from_buffer(
-		userbuf, len, f_pos, buffer,
-		2 * generic_service_priv->resp_size_bytes + 3);
+	mpfs_generic_service_free_message_structs();
+	devm_kfree(generic_service_priv->dev, buffer);
+
+	return ret;
 }
 
 static int mpfs_generic_service_open(struct inode *inode, struct file *filp)
@@ -153,16 +176,17 @@ static int mpfs_generic_service_probe(struct platform_device *pdev)
 			"Failed to find mpfs system controller node\n");
 		return -ENODEV;
 	}
-
+	
 	generic_service_priv->sys_controller =  mpfs_sys_controller_get(&pdev->dev, node_pointer);
 	of_node_put(node_pointer);
 	if (!generic_service_priv->sys_controller)
 		return -EPROBE_DEFER;
 
+	generic_service_priv->dev = dev;
+
 	platform_set_drvdata(pdev, generic_service_priv);
 	misc_register(&mpfs_generic_service_dev);
-	dev_info(&pdev->dev,
-		 "Successfully registered mpfs generic_service driver\n");
+	dev_info(dev, "Successfully registered mpfs generic_service driver\n");
 
 	return 0;
 }
