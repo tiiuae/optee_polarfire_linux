dcbcee6ad43005280ba0c165ce48000b56e2a01f
Ivan Kuznetsov <jsvapiav@gmail.com>
2023-04-03 17:32:12 +0300

    sel4com: add page for circular buffer control data
    Circular buffer size needs to be exactly power-of-2. Additional
    page reserved for buffers control and header data.
    
    Signed-off-by: Tero Salminen <tero.salminen@unikie.com>
    Change-Id: I74b0282726cef64fade007c1affdb317b35b0bde
    
    Signed-off-by: Ivan Kuznetsov <jsvapiav@gmail.com>

--- drivers/misc/sel4com.c
+++ drivers/misc/sel4com.c
@@ -16,23 +16,26 @@
 #include <linux/err.h>
 #include <linux/atomic.h>
 
+#define DT_COMM_CTRL		"ctrl"
 #define DT_REE2TEE_NAME		"ree2tee"
 #define DT_TEE2REE_NAME		"tee2ree"
 
+/****  Common definitions with REE  ****/
 #define COMM_MAGIC_TEE		0x87654321
 #define COMM_MAGIC_REE		0xFEDCBA98
 
-struct comm_ch {
+struct tee_comm_ctrl {
 	uint32_t ree_magic;
 	uint32_t tee_magic;
-	uint32_t head;
-	uint32_t tail;
-	uint8_t data[0];
+	int32_t head;
+	int32_t tail;
 };
+/***************************************/
 
 struct tee_comm_ch {
+	struct tee_comm_ctrl *ctrl;
 	size_t buf_len;
-	struct comm_ch *ch;
+	char *buf;
 };
 
 struct sel4com {
@@ -45,38 +48,23 @@ static ssize_t diag_show(struct device *dev, struct device_attribute *attr,
 {
 	int pos = 0;
 	struct sel4com *priv = dev_get_drvdata(dev);
-
 	pos += sysfs_emit_at(buf, pos, "REE->TEE\n");
-	pos += sysfs_emit_at(buf, pos, "ree_magic: 0x%x\n", priv->ree2tee.ch->ree_magic);
-	pos += sysfs_emit_at(buf, pos, "tee_magic: 0x%x\n", priv->ree2tee.ch->tee_magic);
-	pos += sysfs_emit_at(buf, pos, "head: %d\n", priv->ree2tee.ch->head);
-	pos += sysfs_emit_at(buf, pos, "tail: %d\n", priv->ree2tee.ch->tail);
+	pos += sysfs_emit_at(buf, pos, "ree_magic: 0x%x\n", priv->ree2tee.ctrl->ree_magic);
+	pos += sysfs_emit_at(buf, pos, "tee_magic: 0x%x\n", priv->ree2tee.ctrl->tee_magic);
+	pos += sysfs_emit_at(buf, pos, "head: %d\n", priv->ree2tee.ctrl->head);
+	pos += sysfs_emit_at(buf, pos, "tail: %d\n", priv->ree2tee.ctrl->tail);
 
 	pos += sysfs_emit_at(buf, pos, "TEE->REE\n");
-	pos += sysfs_emit_at(buf, pos, "ree_magic: 0x%x\n", priv->tee2ree.ch->ree_magic);
-	pos += sysfs_emit_at(buf, pos, "tee_magic: 0x%x\n", priv->tee2ree.ch->tee_magic);
-	pos += sysfs_emit_at(buf, pos, "head: %d\n", priv->tee2ree.ch->head);
-	pos += sysfs_emit_at(buf, pos, "tail: %d\n", priv->tee2ree.ch->tail);
+	pos += sysfs_emit_at(buf, pos, "ree_magic: 0x%x\n", priv->tee2ree.ctrl->ree_magic);
+	pos += sysfs_emit_at(buf, pos, "tee_magic: 0x%x\n", priv->tee2ree.ctrl->tee_magic);
+	pos += sysfs_emit_at(buf, pos, "head: %d\n", priv->tee2ree.ctrl->head);
+	pos += sysfs_emit_at(buf, pos, "tail: %d\n", priv->tee2ree.ctrl->tail);
 
 	return pos;
 }
 static DEVICE_ATTR_RO(diag);
 
-static ssize_t inc_head_store(struct device *dev, struct device_attribute *attr,
-			    const char *buf, size_t count)
-{
-	struct sel4com *priv = dev_get_drvdata(dev);
-
-	priv->ree2tee.ch->head += count;
-	dev_info(dev, "ree2tee.ch->head: %d", priv->ree2tee.ch->head);
-
-	return count;
-}
-
-static DEVICE_ATTR_WO(inc_head);
-
 static struct attribute *sel4com_sysfs_attrs[] = {
-	&dev_attr_inc_head.attr,
 	&dev_attr_diag.attr,
 	NULL
 };
@@ -85,57 +73,99 @@ static struct attribute_group sel4com_attribute_group = {
 	.attrs = sel4com_sysfs_attrs
 };
 
-static int sel4com_ch_setup(struct platform_device *pdev, const char *dt_name, struct tee_comm_ch *channel)
+static int sel4com_dt_config(struct device *dev, const char *dt_name,
+			     void **vaddr, size_t *len)
 {
 	int err = -1;
-	struct device_node *np = dev_of_node(&pdev->dev);
+	struct device_node *np = dev_of_node(dev);
 	struct device_node *dt_ch = NULL;
-	struct resource res = {0};
+	struct resource res = { 0 };
 	void *ch_base = NULL;
 
 	dt_ch = of_get_child_by_name(np, dt_name);
 	if (!dt_ch) {
-		dev_err(&pdev->dev, "channel %s not found", dt_name);
+		dev_err(dev, "channel %s not found", dt_name);
 		return -ENXIO;
 	}
 
 	err = of_address_to_resource(dt_ch, 0, &res);
 	if (err) {
-		dev_err(&pdev->dev, "%s no memory area defined", dt_name);
+		dev_err(dev, "%s no memory area defined", dt_name);
 		return err;
 	}
 
-	ch_base = devm_ioremap_resource(&pdev->dev, &res);
+	ch_base = devm_ioremap_resource(dev, &res);
 	err = PTR_ERR_OR_ZERO(ch_base);
 	if (err) {
-		dev_err(&pdev->dev, "%s memory map failed %d", dt_name, err);
+		dev_err(dev, "%s memory map failed %d", dt_name, err);
 		return err;
 	}
 
-	channel->ch = ch_base;
-	channel->buf_len = resource_size(&res) - sizeof(struct tee_comm_ch);
+	*vaddr = ch_base;
+	*len = resource_size(&res);
 
-	dev_info(&pdev->dev, "%s %pr", dt_name, &res);
+	dev_info(dev, "%s %pr", dt_name, &res);
+
+	return err;
+}
+
+static int sel4com_ch_setup(struct device *dev, struct sel4com *priv)
+{
+	int err = -1;
+	void *base = 0;
+	size_t len = 0;
+
+	err = sel4com_dt_config(dev, DT_COMM_CTRL, &base, &len);
+	if (err)
+		return err;
+
+	if (len < (sizeof(struct tee_comm_ctrl) * 2)) {
+		dev_err(dev, "ctrl buffer too small %ld", len);
+		return -ENOBUFS;
+	}
+
+	/* First area is for REE->TEE buffer control data and
+	 * second is for TEE->REE.
+	 */
+	priv->ree2tee.ctrl = (struct tee_comm_ctrl *)base;
+	base += sizeof(struct tee_comm_ctrl);
+	priv->tee2ree.ctrl = (struct tee_comm_ctrl *)base;
+
+	err = sel4com_dt_config(dev, DT_REE2TEE_NAME, &base, &len);
+	if (err)
+		return err;
+
+	priv->ree2tee.buf = (char *)base;
+	priv->ree2tee.buf_len = len;
+
+	err = sel4com_dt_config(dev, DT_TEE2REE_NAME, &base, &len);
+	if (err)
+		return err;
+
+	priv->tee2ree.buf = (char *)base;
+	priv->tee2ree.buf_len = len;
 
 	return err;
 }
 
 static void sel4com_init_ch(struct sel4com *priv)
 {
-	/* producer initializes the channel */
-	memset(priv->ree2tee.ch->data, 0x0, priv->ree2tee.buf_len);
+	/* writer initializes the channel */
+	memset(priv->ree2tee.buf, 0x0, priv->ree2tee.buf_len);
 
-	priv->ree2tee.ch->head = 0;
-	priv->ree2tee.ch->tail = 0;
-	priv->tee2ree.ch->ree_magic = COMM_MAGIC_REE;
+	priv->ree2tee.ctrl->head = 0;
+	priv->ree2tee.ctrl->tail = 0;
 
-	/* Writing magic indicates channel setup is ready */
-	smp_store_release(&priv->ree2tee.ch->ree_magic, COMM_MAGIC_REE);
+	/* Writing magic indicates channel setup is ready. SMP release
+	 * semantics to ensure init operations are done prior.
+	 */
+	smp_store_release(&priv->tee2ree.ctrl->ree_magic, COMM_MAGIC_REE);
+	priv->ree2tee.ctrl->ree_magic = COMM_MAGIC_REE;
 }
 
 static int sel4com_probe(struct platform_device *pdev)
 {
-	int err = 0;
+	int err = -1;
 	struct sel4com *priv = NULL;
 
 	priv = devm_kzalloc(&pdev->dev, sizeof(*priv), GFP_KERNEL);
@@ -144,11 +174,7 @@ static int sel4com_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, priv);
 
-	err = sel4com_ch_setup(pdev, DT_REE2TEE_NAME, &priv->ree2tee);
-	if (err)
-		return err;
-
-	err = sel4com_ch_setup(pdev, DT_TEE2REE_NAME, &priv->tee2ree);
+	err = sel4com_ch_setup(&pdev->dev, priv);
 	if (err)
 		return err;
 
